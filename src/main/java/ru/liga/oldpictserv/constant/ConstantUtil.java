f64cbed95d5e1d22655a4745e01ba089c9befd0c
Maksim Dunaev <DunaevMaxD@gmail.com>
2022-07-15 16:54:42 +0300

    Fix font

--- src/main/java/ru/liga/oldpictserv/constant/ConstantUtil.java
+++ src/main/java/ru/liga/oldpictserv/constant/ConstantUtil.java
@@ -4,8 +4,8 @@ import java.awt.*;
 
 public class ConstantUtil {
     public static final String BACKGROUND_PATH = "templates/background.jpg";
-    public static final String OLDSTANDART_BOLD_PATH = "/font/OldStandardTT-Bold.ttf";
-    public static final String OLDSTANDART_REG_PATH = "/font/OldStandardTT-Regular.ttf";
+    public static final String OLDSTANDART_BOLD_PATH = "font/OldStandardTT-Bold.ttf";
+    public static final String OLDSTANDART_REG_PATH = "font/OldStandardTT-Regular.ttf";
     public static final Integer PADDING = 25;
     public static final Color COLOUR_TEXT = Color.BLACK;
     public static final Integer DEF_FONT_BOLD = 50;
